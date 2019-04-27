26545a3dc10c8377e5da26fc18039f4ba7ac4e3e
N8 <n8@n8programs.com>
2019-04-27 13:55:28 -0400

    Bug fix

--- server/express-setup.js
+++ server/express-setup.js
@@ -12,7 +12,8 @@ app.use(express.static(publicPath));
 app.use(bodyParser.urlencoded());
 let server = http.createServer(app);
 let io = socketIO(server);
-MongoClient.connect("mongodb://theguchi:bon123@ds149056.mlab.com:49056/heroku_hjzx516b" || "mongodb://localhost:27017/beSoMusical", (err, client) => { mongoHandler(err, client, app, process.env.MONGODB_URI) });
+process.env.MONGODB_URI = "mongodb://theguchi:bon123@ds149056.mlab.com:49056/heroku_hjzx516b";
+MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/beSoMusical", (err, client) => { mongoHandler(err, client, app, process.env.MONGODB_URI) });
 
 
 module.exports.listen = port => {
