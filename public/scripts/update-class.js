cfb44be6faa024cdf47ce8af75bb9c7cffd05e66
N8 <n8@n8programs.com>
2019-05-11 21:57:36 -0400

    Fix student permissions

--- public/scripts/update-class.js
+++ public/scripts/update-class.js
@@ -1,12 +1,16 @@
+import { user } from "./main.js";
+
 export default (e, className, that) => {
-    const bar = $($(that).children().get(0));
-    const xDiff = e.clientX - bar.offset().left;
-    const percent = xDiff / $(that).width() * 100;
-    bar.css("width", `${Math.ceil(percent)}%`);
-    bar.html(`${bar.attr("piecename")} : ${Math.ceil(percent)}% Progress`);
-    $.post("/update-piece", {
-        className,
-        pieceName: bar.attr("piecename"),
-        progress: percent
-    });
+    if (user.type === "Teacher") {
+        const bar = $($(that).children().get(0));
+        const xDiff = e.clientX - bar.offset().left;
+        const percent = xDiff / $(that).width() * 100;
+        bar.css("width", `${Math.ceil(percent)}%`);
+        bar.html(`${bar.attr("piecename")} : ${Math.ceil(percent)}% Progress`);
+        $.post("/update-piece", {
+            className,
+            pieceName: bar.attr("piecename"),
+            progress: percent
+        });
+    }
 }
