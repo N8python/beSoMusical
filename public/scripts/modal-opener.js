7a1225dc831d27fb78c43d8cdf5af6661e288f36
N8 <n8@n8programs.com>
2019-05-14 21:26:16 -0400

    Updated Modals

--- public/scripts/modal-opener.js
+++ public/scripts/modal-opener.js
@@ -3,13 +3,13 @@ export default (path, callback = () => {}) => {
         const modal = $(`<div class="w3-modal w3-animate-zoom"></div>`);
         const container = $(`<div class="w3-modal-content w3-padding">`);
         modal.css("display", "block");
+        const remove = $(`<button class="w3-right w3-round w3-btn w3-gray">`).html("x");
+        container.append(remove);
         container.append(data);
         modal.append(container);
-        $(window).click(() => {
-            if (!container[0].contains(event.target) && !(e.target.tagName === "BUTTON")) {
-                modal.css("display", "none");
-                modal.remove();
-            }
+        remove.click(() => {
+            modal.css("display", "none");
+            modal.remove();
         });
         $("body").append(modal);
         callback(modal);
